f6c1bfaa0a79c5bdf58debe4fba6d51a4963d6a0
ChristianRhey Tojot <christiantojot@gmail.com>
2023-06-06 21:57:45 -0700

    debug 3

---
+++ src/Hornedbeast.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import HornedBeastCard from './HornedBeastCard';
+
+
+class Hornedbeast extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            votes: 0
+        }
+
+    }
+    
+    handleVotes = () => {
+        this.setState({
+            votes: this.state.votes + 1
+        })
+    }
+    
+    render(){
+        return(
+            <>
+                <HornedBeastCard title={this.props.title} votes={this.state.votes} description={this.props.description} image_url={this.props.image_url} handleVotes={this.handleVotes} />
+            </>
+            
+        )
+    }
+    
+}
+
+export default Hornedbeast;
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import data from './data/data.json';
 import './Main.css'
-import HornedBeast from './HornedBeast';
+import Hornedbeast from './Hornedbeast';
+
 
 class Main extends React.Component {
     render(){
         return (
             <main>
                 {data.map(hornedBeastObj => {
-                    return <HornedBeast key={hornedBeastObj.title} title={hornedBeastObj.title} image_url={hornedBeastObj.image_url} description={hornedBeastObj.description}/>
+                    return <Hornedbeast key={hornedBeastObj.title} title={hornedBeastObj.title} image_url={hornedBeastObj.image_url} description={hornedBeastObj.description}/>
                 })}
             </main>
         )
