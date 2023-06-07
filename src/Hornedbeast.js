27a6ef2cde33124be8fc02f9727eeb211d937181
ChristianRhey Tojot <christiantojot@gmail.com>
2023-06-07 00:25:18 +0000

    started lab 02

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import './Hornedbeast.css'
 
 
 class Hornedbeast extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            waves: 0
+        }
+
+    }
+    
+    handleWave = () => {
+        this.setState({
+            waves: this.state.waves + 1
+        })
+    }
+    
     render(){
         return(
             <>
-                <h2>
-                    <p>{this.props.title}</p>
-                </h2>
+                <h2><p>{this.props.title}</p></h2>
+                <p onClick={}></p>
                 <img src={this.props.img_url} alt={this.props.title} title={this.props.title}></img>
                 <p>{this.props.description}</p>
             </>
